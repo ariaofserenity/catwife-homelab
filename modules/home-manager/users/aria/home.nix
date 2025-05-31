bcbfffa984af3d49ea577d39112c2d70c84ed382
aria <2664476+ariaofserenity@users.noreply.github.com>
2025-05-31 18:25:57 -0400

    test

--- modules/home-manager/users/aria/home.nix
+++ modules/home-manager/users/aria/home.nix
@@ -7,6 +7,9 @@ in
   home-manager.users.aria = { pkgs, ... }: {
     home.packages = [ pkgs.atool ];
     programs.bash.enable = true;
+    home.shellAliases = {
+      g = "git";
+    };
   
     home.stateVersion = "25.05";
   };
