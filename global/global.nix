3e44df91fced69fc724f1a82f2f97f0368ad6342
aria <2664476+ariaofserenity@users.noreply.github.com>
2025-06-21 15:32:56 -0400

    added auto-update

--- global/global.nix
+++ global/global.nix
@@ -1,65 +1,70 @@
 { config, lib, pkgs, ... }:
 
 {
-    imports =  [
-        ../users/aria.nix
+  imports = [ ../users/aria.nix ];
+  time.timeZone = "America/New_York";
+  services.xserver.xkb.layout = "us";
+
+  networking = {
+    nameservers = [ "192.168.2.5" "1.1.1.1" ];
+    search = [ "catwife.dev" ];
+  };
+
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  services.openssh = {
+    enable = true;
+    settings.PasswordAuthentication = false;
+  };
+
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 7d";
+  };
+
+  environment.systemPackages = with pkgs; [
+    fail2ban
+    python3
+    git
+    wget
+    curl
+    age
+    sops
+    killall
+    dig
+  ];
+
+  system.autoUpgrade = {
+    enable = true;
+    flake = inputs.self.outPath;
+    flags = [
+      "--update-input"
+      "nixpkgs"
+      "-L"
     ];
-    time.timeZone = "America/New_York";
-    services.xserver.xkb.layout = "us";
+    dates = "02:00";
+    randomizedDelaySec = "45min";
+  };
 
-    networking = {
-        nameservers = [
-      "192.168.2.5"
-      "1.1.1.1"
-    ];
-    search = [
-        "catwife.dev"
-    ];
+  sops.age.keyFile = "/etc/age/age.key";
+
+  virtualisation.docker.enable = true;
+
+  services.fail2ban = {
+    enable = true;
+    maxretry = 5;
+    bantime = "24h";
+    bantime-increment = {
+      enable = true;
+      multipliers = "1 2 4 8 16 32 64";
+      maxtime = "168h";
+      overalljails = true;
     };
-    
-    nix.settings.experimental-features = ["nix-command" "flakes" ];
-    
-    boot.loader.systemd-boot.enable = true;
-    boot.loader.efi.canTouchEfiVariables = true;
-    
-    services.openssh = {
-        enable = true;
-        settings.PasswordAuthentication = false;
-    };
+  };
 
-    nix.gc = {
-                automatic = true;
-                dates = "weekly";
-                options = "--delete-older-than 7d";
-    };
-
-    environment.systemPackages = with pkgs; [
-        fail2ban
-        python3
-        git
-        wget
-        curl
-        age
-        sops
-        killall
-        dig
-    ];
-
-    sops.age.keyFile = "/etc/age/age.key";
-
-    virtualisation.docker.enable = true;
-
-    services.fail2ban = {
-        enable = true;
-        maxretry = 5;
-        bantime = "24h";
-        bantime-increment = {
-        enable = true;
-        multipliers = "1 2 4 8 16 32 64";
-        maxtime = "168h";
-        overalljails = true;
-        };
-    };
-
-    system.stateVersion = "25.05"; 
+  system.stateVersion = "25.05";
 }
